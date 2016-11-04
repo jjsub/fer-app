1d640c9e676721ca2f0f5e4763f14d2e9e924f44
jjsub <jsamesub@gmail.com>
2016-11-04 14:10:12 -0500

    Comment table

--- src/tests.js
+++ src/tests.js
@@ -20,34 +20,36 @@ export default class TestsLogs extends React.Component {
 
     return (
       <div>
-      <ul className="log-border">
-        {listOfLogs}
-      </ul>
-      <table class="u-full-width">
-    <thead>
-      <tr>
-        <th>Status</th>
-        <th>Age</th>
-        <th>Sex</th>
-        <th>Location</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>Dave Gamache</td>
-        <td>26</td>
-        <td>Male</td>
-        <td>San Francisco</td>
-      </tr>
-      <tr>
-        <td>Dwayne Johnson</td>
-        <td>42</td>
-        <td>Male</td>
-        <td>Hayward</td>
-      </tr>
-    </tbody>
-  </table>
-</div>
+        <ul className="log-border">
+          {listOfLogs}
+        </ul>
+      </div>
     );
   }
 }
+
+
+// <table class="u-full-width">
+// <thead>
+// <tr>
+//   <th>Status</th>
+//   <th>Age</th>
+//   <th>Sex</th>
+//   <th>Location</th>
+// </tr>
+// </thead>
+// <tbody>
+// <tr>
+//   <td>Dave Gamache</td>
+//   <td>26</td>
+//   <td>Male</td>
+//   <td>San Francisco</td>
+// </tr>
+// <tr>
+//   <td>Dwayne Johnson</td>
+//   <td>42</td>
+//   <td>Male</td>
+//   <td>Hayward</td>
+// </tr>
+// </tbody>
+// </table>
